d361a5c42e8e9927d84b9ce0994fd4f81a805b8a
jonathon hawkins <jonathonhawkins92@gmail.com>
2021-03-17 19:02:41 +0000

    removing old project files

--- src/TooMany.WebClient/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './useRealtime';
-export { default as useApi } from './useAPI';
-export { default as useFetcher } from './useFetcher';
-export { default as useToggle } from './useToggle';
-export { default as useTaskMeta } from './useTaskMeta';
-export { default as useDeserializedLocalStorage } from './useDeserializedLocalStorage';
--- src/TooMany.WebClient/hooks/useDeserializedLocalStorage.ts
+++
@@ -1,6 +0,0 @@
-export default function <T>(key: string) {
-	const serialized = localStorage.getItem(key);
-	if (!serialized) return null;
-	const deserialized: T = JSON.parse(serialized);
-	return deserialized;
-}
