3e53eb14796f435ecef76ddd2a73064b5020a961
jonathon hawkins <jonathonhawkins92@gmail.com>
2021-05-29 19:55:37 +0100

    tidying up terminal a little

--- src/TooMany.WebClient/src/components/terminal/Terminal.tsx
+++ src/TooMany.WebClient/src/components/terminal/Terminal.tsx
@@ -28,9 +28,11 @@ export default function ({
 		const webglAddon = new WebglAddon();
 		instance.loadAddon(webglAddon);
 
-		instance.write(
-			logs.map((log) => `\r\n${log.timestamp} - ${log.text}`).join(),
-		);
+		const pastLogs = logs
+			.map((log) => `${log.timestamp} - ${log.text}\r\n`)
+			.join();
+		instance.write(pastLogs);
+
 		const resizeEvent = () => fitAddon.fit();
 		resizeEvent();
 		window.addEventListener('resize', resizeEvent);
@@ -41,53 +43,3 @@ export default function ({
 
 	return <div style={{ flex: '1 100%' }} ref={containerRef} />;
 }
-
-// export default (function ({
-// 	logs,
-// 	isTaskNameVisible = false,
-// }: {
-// 	logs: Task.ILog[];
-// 	isTaskNameVisible?: boolean;
-// }) {
-// 	return (
-// 		<div className="divide-y divide-gray-600">
-// 			{logs.map((log, index) => (
-// 				<Log
-// 					key={`${log.task}/${index}`}
-// 					isEven={index % 2 === 0}
-// 					task={log.task}
-// 					timestamp={log.formattedTimestamp}
-// 					text={log.text}
-// 					isTaskNameVisible={isTaskNameVisible}
-// 				/>
-// 			))}
-// 		</div>
-// 	);
-// });
-// const Cell = ({ columnIndex, rowIndex, style }) => (
-// 	<div style={style}>
-// 		r{rowIndex}, c{columnIndex}
-// 	</div>
-// );
-
-// export default function ({
-// 	logs,
-// 	isTaskNameVisible = false,
-// }: {
-// 	logs: Task.ILog[];
-// 	isTaskNameVisible?: boolean;
-// }) {
-// 	return (
-// 		<Grid
-// 			className="Grid"
-// 			columnCount={isTaskNameVisible ? 4 : 3}
-// 			columnWidth={() => 200}
-// 			height={1000}
-// 			rowCount={logs.length}
-// 			rowHeight={() => 50}
-// 			width={isTaskNameVisible ? 800 : 600}
-// 		>
-// 			{Cell}
-// 		</Grid>
-// 	);
-// }
