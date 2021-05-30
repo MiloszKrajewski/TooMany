f0a386f680c4537f83742914c8aee78a1d6bc253
jonathon hawkins <jonathonhawkins92@gmail.com>
2021-05-30 19:23:31 +0100

    re working route match hooks

--- src/TooMany.WebClient/src/hooks/Navigation/useIsDefine.tsx
+++
@@ -1,9 +0,0 @@
-import { useMatch } from 'react-router-dom';
-
-import * as routes from '@tm/helpers/routes';
-
-export default function () {
-	const isDefine = useMatch(routes.define());
-	const isRedefine = useMatch(routes.redefine());
-	return isDefine || isRedefine;
-}
--- src/TooMany.WebClient/src/hooks/Navigation/useMonitorParams.tsx
+++ src/TooMany.WebClient/src/hooks/Navigation/useMonitorParams.tsx
@@ -5,8 +5,12 @@ import * as routes from '@tm/helpers/routes';
 
 export default function () {
 	const monitor = useMatch(routes.monitor());
+
 	return useMemo(() => {
-		const params = monitor?.params || {};
+		const params = monitor?.params;
+		if (typeof params === 'undefined') {
+			return undefined;
+		}
 		return { name: params.name, type: params.type };
 	}, [monitor]);
 }
