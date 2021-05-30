576174316565746cbaa3661c8b5bb7122b0d2d1a
jonathon hawkins <jonathonhawkins92@gmail.com>
2021-05-30 19:25:57 +0100

    unifying query and hook for use logs by tag

--- src/TooMany.WebClient/src/hooks/API/Task/Log/useLogsByTag.tsx
+++ src/TooMany.WebClient/src/hooks/API/Task/Log/useLogsByTag.tsx
@@ -8,15 +8,14 @@ import { getTagQueryKey, transformLog } from './helpers';
 import settings from './settings';
 import type { TaskName } from './types';
 
-export function query(
-	taskNames: TaskName[],
-	fetcher: (task: string) => Promise<Task.ILog[]>,
-) {
-	return async function () {
+export default function (taskNames: TaskName[] = [], tag: string) {
+	const api = useApi();
+
+	return useQuery(getTagQueryKey(tag), async function () {
 		const logFetchers = taskNames.map(async (name): Promise<
 			Task.ILog[]
 		> => {
-			const logs = await fetcher(name as string);
+			const logs = await api.task.logs(name as string);
 			const iLogs = logs.map(transformLog(name || ''));
 			if (settings.stripEmptyLogs) {
 				return iLogs.filter((log) => log.text);
@@ -24,16 +23,9 @@ export function query(
 				return iLogs;
 			}
 		});
-
 		const results = await Promise.all(logFetchers);
 
 		const out = results.flat().sort((a, b) => a.time - b.time);
 		return out;
-	};
-}
-
-export default function (taskNames: TaskName[] = [], tag: string) {
-	const api = useApi();
-
-	return useQuery(getTagQueryKey(tag), query(taskNames, api.task.logs));
+	});
 }
