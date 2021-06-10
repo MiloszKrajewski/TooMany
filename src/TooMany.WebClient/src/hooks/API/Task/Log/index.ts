1d41cf01ed6c1d19774c4e4b2c3a6eef42760145
jonathon hawkins <jonathonhawkins92@gmail.com>
2021-06-10 22:20:43 +0100

    removing old logs getter

--- src/TooMany.WebClient/src/hooks/API/Task/Log/index.ts
+++ src/TooMany.WebClient/src/hooks/API/Task/Log/index.ts
@@ -1,4 +1,3 @@
 export { default as useLog } from './useLog';
-export { default as useLogs } from './useLogs';
 export { default as useLogsByTag } from './useLogsByTag';
 export { default as useRealtimeCache } from './useRealtimeCache';
